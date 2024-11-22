311c0dde66f9510172bc1902b2878816e2832e25
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-11-22 14:32:33 -0300

    feat

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@nestjs/common';
-import { EnvConfig } from './env-config.interface';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common'
+import { EnvConfig } from './env-config.interface'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class EnvConfigService implements EnvConfig {
-  constructor(private configService: ConfigService){}
+  constructor(private configService: ConfigService) {}
 
   getAppPort(): number {
-    return Number(this.configService.get<number>("PORT"));
+    return Number(this.configService.get<number>('PORT'))
   }
+
   getNodeEnv(): string {
-    return this.configService.get<string>("NODE_ENV");
+    return this.configService.get<string>('NODE_ENV')
+  }
+
+  getJwtSecret(): string {
+    return this.configService.get<string>('JWT_SECRET')
+  }
+
+  getJwtExpiresInSeconds(): number {
+    return Number(this.configService.get<number>('JWT_EXPIRES_IN'))
   }
 }
